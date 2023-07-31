cb39c5c6c8eeb3805c2351197ddaf914feb87847
H3llBunny <bogo.dishlyanov@gmail.com>
2023-07-31 22:52:09 +0300

    Update

---
+++ Phonebook-Web-Project/Phonebook/Controllers/ContactController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Phonebook.Data;
+using Phonebook.Data.Models;
+using System.Text.RegularExpressions;
+
+namespace Phonebook.Controllers
+{
+    public class ContactController : Controller
+    {
+        [HttpPost]
+        public IActionResult Create (Contact contact)
+        {
+            string namePattern = @"^[A-Z][a-z]+$";
+            string phoneNumPattern = @"^\+359[0-9]{9}$";
+            Regex nameRegex = new Regex(namePattern);
+            Regex phoneRegex = new Regex(phoneNumPattern);
+
+            if (nameRegex.IsMatch(contact.Name) && phoneRegex.IsMatch(contact.Number))
+                {
+                    DataAccess.Contacts.Add(contact);
+                }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Phonebook-Web-Project/Phonebook/Data/Models/Contact.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Phonebook.Data.Models
+{
+    public class Contact
+    {
+        [Required]
+        public string Name { get; set; }
+        public string Number { get; set; }
+    }
+}
